0db12183e3dccbc9ab6205e4b762d8720b9871f5
Madino <65070015@kmitl.ac.th>
2023-10-11 00:37:59 +0700

    option

---
+++ app/api/auth/[...nextauth]/option.ts
@@ -0,0 +1,25 @@
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google"
+import PostgresAdapter from "@auth/pg-adapter"
+import { Pool } from "pg"
+
+const pool = new Pool({
+  host: '161.246.127.24',
+  port: 9077,
+  database: 'dbitag',
+  user: 'clmtbmrw30079bsmnfdwi4ovp',
+  password: 'YcVOt4I2p6X3YTDXNltyKgxN',
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
+})
+
+export const options: NextAuthOptions = {
+    adapter: PostgresAdapter(pool),
+    providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+    })
+  ]
+}
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,6 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import PostgresAdapter from "@auth/pg-adapter"
-import { Pool } from "pg"
+import { options } from "./option"
 
-const pool = new Pool({
-  host: '161.246.127.24',
-  port: 9077,
-  database: 'dbitag',
-  user: 'clmtbmrw30079bsmnfdwi4ovp',
-  password: 'YcVOt4I2p6X3YTDXNltyKgxN',
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000
-})
-
-const handler = NextAuth({
-  adapter: PostgresAdapter(pool),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-    })
-  ]
-})
+const handler = NextAuth(options)
 
 export {handler as GET, handler as POST}
