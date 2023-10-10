58dff9da0d272da320b4e569f24d8e02b3514e26
65070014 <bosspopza150@hotmail.com>
2023-10-10 19:54:19 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -8,52 +8,50 @@ import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 
 import * as React from "react";
-import {NextUIProvider} from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function Home() {
 	return (
-		<NextUIProvider>
-			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-				<div className="inline-block max-w-lg text-center justify-center">
-					<h1 className={title()}>Make&nbsp;</h1>
-					<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-					<br />
-					<h1 className={title()}>
-						websites regardless of your design experience.
-					</h1>
-					<h2 className={subtitle({ class: "mt-4" })}>
-						Beautiful, fast and modern React UI library.
-					</h2>
-				</div>
+		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+			<div className="inline-block max-w-lg text-center justify-center">
+				<h1 className={title()}>Make&nbsp;</h1>
+				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+				<br />
+				<h1 className={title()}>
+					websites regardless of your design experience.
+				</h1>
+				<h2 className={subtitle({ class: "mt-4" })}>
+					Beautiful, fast and modern React UI library.
+				</h2>
+			</div>
 
-				<div className="flex gap-3">
-					<Link
-						isExternal
-						as={NextLink}
-						href={siteConfig.links.docs}
-						className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-					>
-						Documentation
-					</Link>
-					<Link
-						isExternal
-						as={NextLink}
-						className={buttonStyles({ variant: "bordered", radius: "full" })}
-						href={siteConfig.links.github}
-					>
-						<GithubIcon size={20} />
-						GitHub
-					</Link>
-				</div>
+			<div className="flex gap-3">
+				<Link
+					isExternal
+					as={NextLink}
+					href={siteConfig.links.docs}
+					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
+				>
+					Documentation
+				</Link>
+				<Link
+					isExternal
+					as={NextLink}
+					className={buttonStyles({ variant: "bordered", radius: "full" })}
+					href={siteConfig.links.github}
+				>
+					<GithubIcon size={20} />
+					GitHub
+				</Link>
+			</div>
 
-				<div className="mt-8">
-					<Snippet hideSymbol hideCopyButton variant="flat">
-						<span>
-							Get started by editing <Code color="primary">app/page.tsx</Code>
-						</span>
-					</Snippet>
-				</div>
-			</section>
-		</NextUIProvider>
+			<div className="mt-8">
+				<Snippet hideSymbol hideCopyButton variant="flat">
+					<span>
+						Get started by editing <Code color="primary">app/page.tsx</Code>
+					</span>
+				</Snippet>
+			</div>
+		</section>
 	);
 }
