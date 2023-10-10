4cb4bc0b851866452bc31d735e47d1090074bc3f
65070014 <bosspopza150@hotmail.com>
2023-10-10 19:54:53 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,6 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 
-import * as React from "react";
-import { NextUIProvider } from "@nextui-org/react";
-
 export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
