cb44ac11a7a15991ce234a2b583cda8dfd6e344b
65070014 <bosspopza150@hotmail.com>
2023-10-23 12:01:25 +0700

    update ses

--- app/ses/page.tsx
+++ app/ses/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import SesStyle from "./SesStyle.module.css";
-import {ScrollShadow} from "@nextui-org/react";
+import { ScrollShadow,Checkbox } from "@nextui-org/react";
 import ProfileStyle from "@/app/profile/ProfileStyle.module.css";
 
 type Props = {};
@@ -11,30 +11,25 @@ export default function Ses({ }: Props) {
 
     return (
         <ScrollShadow hideScrollBar className="">
-            <div className={`${SesStyle.box} container flex flex-col items-center justify-center mx-auto `}>
-                <br /><br />
-                <div className={`${ProfileStyle.box2} container flex flex-col items-center justify-center mx-auto `}>
-                    <br />
-                    {/*<div className={SesStyle.GridCon}>*/}
-                    {/*    <div className={SesStyle.sesId}>*/}
-                    {/*        Session ID*/}
-                    {/*    </div>*/}
-                    {/*    <div className={SesStyle.master}>*/}
-                    {/*        DG Master*/}
-                    {/*    </div>*/}
-                    {/*    <div className={SesStyle.playerNum}>*/}
-                    {/*        Player*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                    <div className={SesStyle.sesId}>
-                        Session ID
+            <div className={`${SesStyle.box} container flex flex-col items-center justify-center mx-auto font`}>
+                <br />
+                <div style={{ fontSize: '30px' }}>
+                    Session ID [ ]
+                </div>
+                <br />
+                <div className={`${SesStyle.box2} container flex flex-row mx-auto `}>
+                    <div className="flex-1 text-left ">
+                        <h1>DG Master</h1>
+                        <h1><Checkbox defaultSelected size="lg" color="warning"></Checkbox></h1>
                     </div>
-                    <div className={SesStyle.master}>
-                        DG Master
+
+                    <div className="flex-1 text-center">
+                        <h1>Player</h1>
+                        <h1>player1</h1>
                     </div>
-                    <div className={SesStyle.playerNum}>
-                        Player
-                    </div>
+                    
+
+
                     <br /><br /><br /><br />
                 </div>
                 <br /><br />
