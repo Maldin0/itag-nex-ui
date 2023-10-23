fb81f7981cc69f39d380c0770ae35b13c7065b84
65070014 <bosspopza150@hotmail.com>
2023-10-23 17:03:43 +0700

    update alot

--- app/page.tsx
+++ app/page.tsx
@@ -20,11 +20,14 @@ export default function Home() {
               Welcome <br />{session.user.name}
             </h1>
             <div className="py-24 gap-14 w-52" style={{ display: 'flex', flexDirection: 'column', marginRight: '25rem' }}>
+              <div className="text-4xl font" style={{ color: 'white', textShadow: '4px 4px 2px rgba(0, 0, 0, 1)' }}>
+                PLAY AS
+              </div>
               <Button radius="md" size="lg" color="success" variant="solid">
-                create session
+                DUNGEON MASTER
               </Button>
               <Button radius="md" size="lg" color="success" variant="solid">
-                join session
+                PLAYER
               </Button>
             </div>
           </div>
@@ -36,7 +39,7 @@ export default function Home() {
         <>
           <div className="text-between font mb-10" style={{ height: "600px", display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <div className={`container flex flex-row mx-auto `}>
-              <div className="flex-1 text-center text-8xl" style={{color: 'white', textShadow: '7px 7px 4px rgba(0, 0, 0, 1)' }}>
+              <div className="flex-1 text-center text-8xl" style={{ color: 'white', textShadow: '7px 7px 4px rgba(0, 0, 0, 1)' }}>
                 <h1>IT
                   <br />
                   ADVENTURER
